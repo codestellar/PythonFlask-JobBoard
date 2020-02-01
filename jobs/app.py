a2900b4aed35fe9a760575ad48be79a40984a1e0
Gaurav Madaan <gaurav.madaan@technossus.com>
2020-02-01 16:32:28 +0530

    Updated imports

--- jobs/app.py
+++ jobs/app.py
@@ -1,11 +1,11 @@
-from flask import flask, render_function
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/jobs")
 def jobs():
-    return render_function('index.html')
+    return render_template('index.html')
 
 # if __name__ == "__main__":
 #     app.run()
